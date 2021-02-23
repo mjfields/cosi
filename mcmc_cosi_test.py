567d2414c2f548b6cc308d18e6806636bcfba0a0
mjfields <55111366+mjfields@users.noreply.github.com>
2021-02-23 17:13:41 -0500

    Remove extra code from bottom

--- mcmc_cosi_test.py
+++ mcmc_cosi_test.py
@@ -149,74 +149,8 @@ plt.rcParams['savefig.bbox'] = 'tight'
         
 
 
-# c = Cosi(7.3, 0.3, 0.912, 0.034, 6.45, 0.05)
-
-# sampler = c.run_mcmc(100, 1500)
-
-# chain = sampler.get_chain(discard=500, flat=True)
-
-# cosi = chain[:, 0]
-
-# i = np.arccos(cosi) * 180 / np.pi # degrees
-
-# i95 = np.nanpercentile(i, 95)
-
-# i_maxprob = np.arccos(c.get_max_probability(chain)[0]) * 180 / np.pi
-
-# fig = plt.figure()
-
-# plt.hist(i, bins='scott', linewidth=2.5, histtype='step', color='black')
-
-# plt.xlabel("Stellar Inclination ($^{\\circ}$)")
-# plt.ylabel("N")
-
-# fig.savefig('/Users/mjfields/Documents/Research/Disk_Alignment/Analysis/StellarInclination/python_inclination.pdf')
-
-# plt.close()
 
 
 
 
-""" autocorr stuff """
-
-# def next_pow_two(n):
-#     i = 1
-#     while i < n:
-#         i = i << 1
-#     return i
-
-
-# def autocorr_func_1d(x, norm=True):
-#     x = np.atleast_1d(x)
-#     if len(x.shape) != 1:
-#         raise ValueError("invalid dimensions for 1D autocorrelation function")
-#     n = next_pow_two(len(x))
-
-#     # Compute the FFT and then (from that) the auto-correlation function
-#     f = np.fft.fft(x - np.mean(x), n=2 * n)
-#     acf = np.fft.ifft(f * np.conjugate(f))[: len(x)].real
-#     acf /= 4 * n
-
-#     # Optionally normalize
-#     if norm:
-#         acf /= acf[0]
-
-#     return acf
-
-
-# def auto_window(taus, c):
-#     m = np.arange(len(taus)) < c * taus
-#     if np.any(m):
-#         return np.argmin(m)
-#     return len(taus) - 1
-
-
-# def autocorr_new(y, c=5.0):
-#     f = np.zeros(y.shape[1])
-#     for yy in y:
-#         f += autocorr_func_1d(yy)
-#     f /= len(y)
-#     taus = 2.0 * np.cumsum(f) - 1.0
-#     window = auto_window(taus, c)
-#     return taus[window]
 
