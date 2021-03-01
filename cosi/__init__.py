d3a34d409140dee0dfe0bb94485806da91f4f398
Matt Fields <55111366+mjfields@users.noreply.github.com>
2021-03-01 10:36:36 -0500

    Check emcee version

--- cosi/__init__.py
+++ cosi/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import emcee
+import warnings
+from packaging import version
+
 from .core import Probability, CosI
 
 
@@ -9,3 +13,6 @@ __all__ = ["Probability", "CosI"]
 
 
 
+
+if version.parse(emcee.__version__) < version.parse('3.0.0'):
+    warnings.warn("update emcee to at least v3.0.0 or some features may not work")
