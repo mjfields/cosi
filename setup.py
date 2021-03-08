1297559d11effd1173e57b0b750068691634686b
Matt Fields <55111366+mjfields@users.noreply.github.com>
2021-03-08 17:38:54 -0500

    Change version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cosi",
-    version="0.0.1",
+    version="0.1.0",
     author="Matthew J. Fields",
     author_email="fieldsmatthewj@gmail.com",
     description="MCMC estimation of stellar inclination angle",
